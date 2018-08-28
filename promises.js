cf69969cdf3496fbb4e95998e95b1c0312910cc3
MichelleSB1801 <smsb1801@gmail.com>
2018-08-28 20:09:39 -0300

    Ex A

--- promises.js
+++ promises.js
@@ -1,75 +1,28 @@
-// Given the following array of elements:
+var PlayerService = {
+  getPlayerTeamId: function(playerId) {
+          return new Promise(resolve => {
+            $.ajax({
+                  url: "/player/" + playerId + "/team"
+            }).then(response => resolve(response.id));
+          });
+  },
+  getPlayers: function(teamId) {
+          return new Promise(resolve => {
+            $.ajax({
+                  url: "/team/" + teamId + "/player"
+            }).then(response => resolve(response));
+          });
+  }
+};
 
-newItems = [
-        {
-          network: 'facebook',
-          text: 'post number 1',
-        },
-        {
-          network: 'twitter',
-          text: 'some twitter text',
-        },
-        {
-          network: 'gplus',
-          text: 'some gplus stuff',
-        },
-        {
-          network: 'facebook',
-          text: 'post number 2',
-        },
-      ]
-			
-			finalsItems = []
-      
-      // Write a function with this signature:
-      
-      // (arrayOfItems, aNetwork) => newArray
-      
-      function foo(arrayOfItems, aNetwork) {
-				arrayOfItems.map((nw) => {
-					let newItem 
-					if(nw.network == aNetwork){
-						newItem = {
-							'displayName': aNetwork,
-							'text': nw.text,
-						}
-						finalsItems.push(newItem)
-					}
-					
-				})
-			}
-			
+var PlayerDetailsController = {
+  playerId: 8,
+  showTeammatesClick: async function() {
+          teamId = await PlayerService.getPlayerTeamId(this.playerId);
+          playerList = await PlayerService.getPlayers(teamId);
 
-			
-      
-      // That, given an array and a network, transforms the given array into the following structure:
-      
-      // example:
-      // foo(newItems, 'facebook')
-      
-      // outputs:
-      // finalsItems = [
-      //   {
-      //     displayName: 'Facebook'
-      //     text: 'post number 1'
-      //   },
-      //   {
-      //     displayName: 'Facebook',
-      //     text: 'post number 2'
-      //   },
-      // ]
-      
-      // foo(newItems, 'gplus')
-      
-      // outputs:
-      // finalsItems = [
-      //   {
-      //     displayName: 'Google +',
-      //     text: 'some gplus stuff'
-      //   }
-      // ]
-      
-      // You will need to define your own way to consistently transform `network` into `displayNames`
-      
-      console.log(foo(newItems, 'facebook'));
-      console.log(foo(newItems, 'gplus'))
+			//Render playerList
+  }
+};
+
+PlayerDetailsController.showTeammatesClick();
